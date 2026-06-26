1a76003fb5e5635b3cea814b1955082fc2df452f
kavya <kavyachandika@gmail.com>
2026-06-26 14:50:52 -0500

    Create grade_calculator.py

---
+++ grade_calculator.py
@@ -0,0 +1,5 @@
+Exam_1 = float(input("Enter Exam 1 Marks: "))
+Exam_2 = float(input("Enter Exam 2 Marks: "))
+Exam_3 = float(input("Enter Exam 3 Marks: "))
+average = (Exam_1+Exam_2+Exam_3)/3
+print("Your average is ", average)
