e718c3c603ac8050dc9ef6f878cd350a11dd5185
kavya <kavyachandika@gmail.com>
2026-06-30 18:29:26 -0500

    Create numberchecker.py

---
+++ numberchecker.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter your number: "))
+if (num1 == 0):
+    print("your number is zero")
+elif (num1 > 0):
+    print("your number is positive")
+elif (num1 < 0):
+    print("your number is negative")
+else:
+    print("Invalid")
