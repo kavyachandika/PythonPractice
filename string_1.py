43cd31c2dfb962ad2d222be4a47e10f1d0a51e36
kavya <kavyachandika@gmail.com>
2026-07-01 16:59:49 -0500

    Create string_1.py

---
+++ string_1.py
@@ -0,0 +1,10 @@
+name = "kavya "
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+print(name.strip())
+print(name.replace("k", "K"))
+print(name.find("v"))
+print(len(name))
